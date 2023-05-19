22dbb76024c1223f1dc44cf647458b83cfb690e2
eversonnatan <everson.natan@gmail.com>
2023-05-19 20:21:44 -0300

    Add project files.

---
+++ AcessoBD/frmAcesso.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace AcessoBD
+{
+    partial class frmAcesso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblUF = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtUF = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnPesquisa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(2, 30);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(60, 19);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(2, 80);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(122, 19);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome do Estado";
+            // 
+            // lblUF
+            // 
+            this.lblUF.AutoSize = true;
+            this.lblUF.Location = new System.Drawing.Point(2, 150);
+            this.lblUF.Name = "lblUF";
+            this.lblUF.Size = new System.Drawing.Size(124, 19);
+            this.lblUF.TabIndex = 2;
+            this.lblUF.Text = "Unidade Federal:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(6, 50);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(56, 27);
+            this.txtCodigo.TabIndex = 3;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(6, 102);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.ReadOnly = true;
+            this.txtNome.Size = new System.Drawing.Size(287, 27);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtUF
+            // 
+            this.txtUF.Location = new System.Drawing.Point(6, 172);
+            this.txtUF.Name = "txtUF";
+            this.txtUF.ReadOnly = true;
+            this.txtUF.Size = new System.Drawing.Size(56, 27);
+            this.txtUF.TabIndex = 5;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Image = global::AcessoBD.Properties.Resources.exit;
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnExit.Location = new System.Drawing.Point(258, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(70, 70);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "&Sair";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnPesquisa
+            // 
+            this.btnPesquisa.Image = global::AcessoBD.Properties.Resources.search;
+            this.btnPesquisa.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnPesquisa.Location = new System.Drawing.Point(258, 279);
+            this.btnPesquisa.Name = "btnPesquisa";
+            this.btnPesquisa.Size = new System.Drawing.Size(70, 70);
+            this.btnPesquisa.TabIndex = 7;
+            this.btnPesquisa.Text = "&Buscar";
+            this.btnPesquisa.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnPesquisa.UseVisualStyleBackColor = true;
+            this.btnPesquisa.Click += new System.EventHandler(this.btnPesquisa_Click);
+            // 
+            // frmAcesso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 361);
+            this.Controls.Add(this.btnPesquisa);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtUF);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblUF);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblCodigo);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmAcesso";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblUF;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtUF;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnPesquisa;
+    }
+}
+
